dd980da3ed1f712b483d7ff65e16264ec7b0dacd
GamerCleanVic <jonassd2000@gmail.com>
2023-10-10 22:18:24 -0500

    DevDojo ExceptResource

--- javacore/exception/runtimeexception/test/RuntimeExceptionTest.java
+++ javacore/exception/runtimeexception/test/RuntimeExceptionTest.java
@@ -2,22 +2,18 @@ package br.com.abc.javacore.exception.runtimeexception.test;
 
 public class RuntimeExceptionTest {
 	public static void main(String[] args) {
-		
-		int a = 10;
-		int b = 0;
-		Object o = "notNull";
-		
-		if(b != 0) {
-			int c = a / b;
-			System.out.println(c);
+		try {
+			divisao(10, 0);
+		}catch(RuntimeException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}		
-				
-		for (int indexArray = 0; indexArray < 2; indexArray++) {
-			int[] array1 = new int[2];
-			array1[indexArray] = indexArray+1;
-			if(indexArray < 2) {				
-				System.out.println(array1[indexArray]);
-			}
-		}
 	}
+	private static void divisao(int num1, int num2) {
+		if(num2 == 0) {
+			throw new IllegalArgumentException("Passe um valor diferente de zero pra num2.");		}
+		int result = num1 / num2;
+		System.out.println(result);
+	}
+	
 }
