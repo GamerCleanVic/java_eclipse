e05f3f5b9cee51337f89145d1cd26842f7a3c75a
GamerCleanVic <jonassd2000@gmail.com>
2023-10-13 21:35:07 -0500

    wrappers

---
+++ javacore/wrappers/test/WrappersTest.java
@@ -0,0 +1,21 @@
+package br.com.abc.javacore.wrappers.test;
+
+public class WrappersTest {
+	byte bytePrimitivo = 1;
+	short shortPrimitivo = 1;
+	int intPrimitivo = 10;
+	long longPrimitivo = 10;
+	float floatPrimitivo = 10;
+	double doublePrimitivo = 10;
+	char charPrimitivo = 'A';
+	boolean booleanPrimitivo = true;
+	
+	Byte byteWrapper = 1;
+	Short shortWrapper = 1;
+	Integer integerWrapper = 10;
+	Long longWrapper = 10L;
+	Float floatWrapper = 10F;
+	Double doubleWrapper = 10D;
+	Character characterWrapper = 'A';
+	Boolean booleanWrapper = true;
+}
