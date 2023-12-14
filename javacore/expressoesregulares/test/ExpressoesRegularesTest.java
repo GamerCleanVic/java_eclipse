e3a0a4337872cdbc45a518a3971d690618f1e602
GamerCleanVic <jonassd2000@gmail.com>
2023-12-14 16:21:21 -0500

    regex^negação

--- javacore/expressoesregulares/test/ExpressoesRegularesTest.java
+++ javacore/expressoesregulares/test/ExpressoesRegularesTest.java
@@ -18,13 +18,24 @@ public class ExpressoesRegularesTest {
 		// [n, m] de n até m
 		// ( ) agrupar expressão
 		// | ou
-		// $ 
+		// $
+		// . coringa 1.3 = 123, 133, 1A3, 143, 1 3
+		// ^ Negação/Excluir, [^abc]<-Não quer [ab ou c]
+		
 		//Aula 93 - DevDojo
 		
 		int hex = 0x1;
-		String regex = "0[xX]([0-9a-fA-F])*(\\s|$)";
-		String texto = "12 0x 0X 0x01FFABC 0x10G 0x1";
+		//String regex = "0[xX]([0-9a-fA-F])*(\\s|$)";
+		//String texto = "12 0x 0X 0x01FFABC 0x10G 0x1";
 		
+		//String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"; //ou "([\\w\\.])+@([a-zA-Z])+(\\.([a-zA-Z])+)+"
+		//String texto = "fulano@hotmail.com, 102Abc@gmail.com, +@'abrao@mail, teste@gmail.com.br, teste@mail'";		
+		//String regex = "\\d{2}/\\d{2}/\\d{2,4}";
+		//String texto = "05/10/2010 05/05/2015 1/1/01 01/05/05"; //DD/MM/YYYY
+		String regex = "proj([^,])*";
+		String texto = "proj1.bkp, proj1.java, proj1.class, proj1final.java, proj2.bkp, proj3.java, diagrama, texto, foto";
+		
+		//System.out.println("Email válido? "+"+@!abrao@mail.br".matches(regex));
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(texto);
 		
