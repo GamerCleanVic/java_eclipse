8282eaaa9755ebe66f04beb24f0af4d77c7bd474
GamerCleanVic <jonassd2000@gmail.com>
2024-02-08 21:52:33 -0300

    NIO relativize 05 DevDojo

--- javacore/nio/test/RelativizarTest.java
+++ javacore/nio/test/RelativizarTest.java
@@ -11,5 +11,15 @@ public class RelativizarTest {
 		System.out.println(pathToClasse);
 		
 		Path absoluto1 = Paths.get("/home/fulano");
+		Path absoluto2 = Paths.get("/usr/local");
+		Path absoluto3 = Paths.get("/home/fulano/java/Pessoa.java");
+		Path relativo1 = Paths.get("temp");
+		Path relativo2 = Paths.get("temp/Funcionario.java");
+		
+		System.out.println("1: "+absoluto1.relativize(absoluto3));
+		System.out.println("2: "+absoluto3.relativize(absoluto1));
+		System.out.println("3: "+absoluto1.relativize(absoluto2));
+		System.out.println("4: "+relativo1.relativize(relativo2));
+		//System.out.println("5: "+absoluto1.relativize(relativo1));
 	}
 }
