da124d63d24a77d47402915235aaaf34c783b408
GamerCleanVic <jonassd2000@gmail.com>
2024-07-24 15:11:43 -0300

    colections - DevDojo116

--- JavaOnline/src/br/com/abc/javacore/colecoes/test/EqualsTest.java
+++ JavaOnline/src/br/com/abc/javacore/colecoes/test/EqualsTest.java
@@ -1,5 +1,7 @@
 package br.com.abc.javacore.colecoes.test;
 
+import br.com.abc.javacore.colecoes.classes.Celular;
+
 public class EqualsTest {
 	public static void main(String[] args) {
 		String nome1 = "William Suane";
@@ -7,7 +9,11 @@ public class EqualsTest {
 		Integer int1 = 5;
 		Integer int2 = new Integer(5);
 		
-		System.out.println(nome1.equals(nome2));
-		System.out.println(int1.equals(int2));
+		//System.out.println(nome1.equals(nome2));
+		//System.out.println(int1.equals(int2));
+		
+		Celular c1 = new Celular("iPhone", "1234");
+		Celular c2 = new Celular("iPhone", "1234");
+		System.out.println(c1.equals(c2));
 	}
 }
