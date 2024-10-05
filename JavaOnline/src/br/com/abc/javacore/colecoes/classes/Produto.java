d60e086934752bd901e65dc06c93a41f1101848f
GamerCleanVic <jonassd2000@gmail.com>
2024-10-05 13:03:54 -0300

    Iterator - DevDojo

--- JavaOnline/src/br/com/abc/javacore/colecoes/classes/Produto.java
+++ JavaOnline/src/br/com/abc/javacore/colecoes/classes/Produto.java
@@ -5,25 +5,30 @@ import java.util.Objects;
 public class Produto implements Comparable<Produto>{
 	private String serialNumber;
 	private String nome;
-	private Double preco;		
-	
-	
+	private Double preco;
+	private int quantidade;
 	
 	public Produto(String serialNumber, String nome, double preco) {
 		//super();
 		this.serialNumber = serialNumber;
 		this.nome = nome;
 		this.preco = preco;
-	}
+	}	
 
+	public Produto(String serialNumber, String nome, Double preco, int quantidade) {
+		//super();
+		this.serialNumber = serialNumber;
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}	
+		
 	@Override
 	public String toString() {
-		return "Produto ["
-				+ "serialNumber=" + serialNumber + 
-				", nome=" + nome + 
-				", preco=" + preco + "]";
+		return "Produto [serialNumber=" + serialNumber + ", nome=" + nome + ", preco=" + preco + ", quantidade="
+				+ quantidade + "]";
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(serialNumber);
@@ -58,7 +63,16 @@ public class Produto implements Comparable<Produto>{
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
-	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
 	@Override
 	public int compareTo(Produto outroObjeto) {
 		// TODO Auto-generated method stub
