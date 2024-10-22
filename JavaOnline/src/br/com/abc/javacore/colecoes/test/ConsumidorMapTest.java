7fbf08c5d32426baf90f5c02578d1c2db16bc2a9
GamerCleanVic <jonassd2000@gmail.com>
2024-10-21 22:27:14 -0300

    entrySet - DevDojo #129

--- JavaOnline/src/br/com/abc/javacore/colecoes/test/ConsumidorMapTest.java
+++ JavaOnline/src/br/com/abc/javacore/colecoes/test/ConsumidorMapTest.java
@@ -1,6 +1,8 @@
 package br.com.abc.javacore.colecoes.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import br.com.abc.javacore.colecoes.classes.Consumidor;
@@ -15,11 +17,22 @@ public class ConsumidorMapTest {
 		Produto produto3 = new Produto("879", "Teclado Razer", 1000.0);
 		Produto produto4 = new Produto("012", "Samsung Galaxy S7 64GB", 3250.5);
 		
-		Map<Consumidor, Produto> map = new HashMap<>();
-		map.put(consumidor, produto2);
-		map.put(consumidor2, produto4);
-		for(Consumidor item : ) {
-			
+		Map<Consumidor, List<Produto>> map = new HashMap<>();
+		
+		List<Produto> produtosCon1 = new ArrayList<>();
+		produtosCon1.add(produto1);
+		produtosCon1.add(produto2);
+		List<Produto> produtosCon2 = new ArrayList<>();
+		produtosCon2.add(produto3);
+		produtosCon2.add(produto4);
+		
+		map.put(consumidor, produtosCon1);
+		map.put(consumidor2, produtosCon2);
+		for(Map.Entry<Consumidor, List<Produto>> entry : map.entrySet()) {
+			System.out.println(entry.getKey().getNome());
+			for(Produto produto : entry.getValue()) {
+				System.out.println(produto.getNome());
+			}
 		}
 	}
 }
