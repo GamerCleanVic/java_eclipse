eb22f8a5b0b7ed1bc9da6094b081547815d53683
GamerCleanVic <jonassd2000@gmail.com>
2024-11-28 19:45:13 -0300

    Classe Interna Local - DevDojo #137

---
+++ JavaOnline/src/br/com/abc/javacore/classesinternas/test/ClassesLocaisTest.java
@@ -0,0 +1,20 @@
+package br.com.abc.javacore.classesinternas.test;
+
+public class ClassesLocaisTest {
+	private String nome = "William";
+	
+	public void fazAlgumaCoisa() {
+		class Interna{
+			public void imprimeNomeExterno() {
+				System.out.println(nome);
+			}
+		}
+		Interna in = new Interna();
+		in.imprimeNomeExterno();
+	}
+	
+	public static void main (String[] args) {
+		ClassesLocaisTest externa = new ClassesLocaisTest();
+		externa.fazAlgumaCoisa();;
+	}
+}
